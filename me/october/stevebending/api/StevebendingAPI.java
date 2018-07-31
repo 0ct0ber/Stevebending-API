89607d24a9c8941f489e72723396a8706a3901b6
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-07-31 11:54:23 +0100

    Created StevebendingAPI.java

---
+++ me/october/stevebending/api/StevebendingAPI.java
@@ -0,0 +1,83 @@
+package com.october.stevebending.api;
+
+/**
+*Part of Stevebending API
+*Project License: https://github.com/0ct0ber/Stevebending-API/blob/master/LICENSE.txt
+*/
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**Main class for the plugin*/
+
+public final class StevebendingAPI extends JavaPlugin {
+	
+	static Bending bending = null;
+	
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		File playerdata = getPlayerData();
+		if (playerdata == null) {
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		
+		BendingStorage storage = createStorage(playerdata);
+		if (storage == null) {
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		
+		bending = new Bending(storage);
+	}
+	
+	@Override
+	public void onDisable() {
+		bending = null;
+	}
+	
+	private File getPlayerData() {
+		saveDefaultConfig();
+		File folder = this.getDataFolder();
+		if (!folder.exists()) try {
+			folder.createNewFile();
+			folder.mkdir();
+		} catch (IOException ex) {
+			getLogger().warning("An I/O exception occurred while trying to create the plugin folder, disabling: ");
+			ex.printStackTrace();
+			return null;
+		}
+		
+		if (!folder.isDirectory()) {
+			getLogger().warning("Plugin folder is not a folder, disabling");
+			return null;
+		}
+		
+		File playerdata = new File(folder, "playerdata.yml");
+		if (!playerdata.exists()) try {
+			playerdata.createNewFile();
+		}  catch (IOException ex) {
+			getLogger().warning("An I/O exception occurred while trying to create the playerdata file, disabling: ");
+			ex.printStackTrace();
+			return null;
+		}
+		return playerdata;
+	}
+	
+	private BendingStorage createStorage(File playerdata) {
+		BendingStorage storage = null;
+		try {
+			storage = new YamlBendingStorage(playerdata, getLogger());
+		} catch (RuntimeException ex) {
+			getLogger().warning("An exception occurred while trying to create the bending storage, disabling plugin: ");
+			ex.printStackTrace();
+		}
+		
+		return storage;
+	}
+
+}
