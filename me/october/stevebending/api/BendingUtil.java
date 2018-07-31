4594f06c3c02ff6fb54f29556e34ba1a16b36369
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-07-31 14:32:29 +0100

    Updated BendingUtil.java
    Added checks for the various element types

--- me/october/stevebending/api/BendingUtil.java
+++ me/october/stevebending/api/BendingUtil.java
@@ -5,12 +5,33 @@
 
 package com.october.stevebending.api;
 
+import static com.october.stevebending.api
 public abstract class BendingUtil {
 	
 	protected BendingUtil() {}
 	
 	protected static final Bending getBending() {
-		return StevebendingAPI.bending;
+		return StevebendingAPI.getBending();
+	}
+	
+	public static boolean isNonBender(Player player) {
+		return getBending().getBendingType(player) == BendingType.NONE;
+	}
+	
+	public static boolean isWaterbender(Player player) {
+		return getBending().getBendingType(player) == BendingType.WATER;
+	}
+	
+	public static boolean isFirebender(Player player) {
+		return getBending().getBendingType(player) == BendingType.FIRE;
+	}
+	
+	public static final boolean isEarthbender(Player player) {
+		return getBending().getBendingType(player) == BendingType.EARTH;
+	}
+	
+	public static final boolean isAirbender(Player player) {
+		return getBending().getBendingType(player) == BendingType.AIR;
 	}
 
 }
